e42ec74a54d1c0c3652b86d422f6b4b2e53bc004
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-05-11 19:05:17 +0530

    fix: scroll to top only first time the page load

--- src/components/common/ScrollToTop.jsx
+++ src/components/common/ScrollToTop.jsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 const ScrollToTopLayout = () => {
   const location = useLocation();
+  const visitedRoutes = useRef(new Set());
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    const path = location.pathname;
+
+    if (!visitedRoutes.current.has(path)) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      visitedRoutes.current.add(path);
+    }
   }, [location.pathname]);
 
   return <Outlet />;
